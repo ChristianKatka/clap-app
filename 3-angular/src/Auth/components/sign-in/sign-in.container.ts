97778399ec0036d8cbfbecbc7da23e46bb1f6d08
Christian Katka <christian.katka@skillwell.fi>
2021-11-30 09:56:05 +0200

    sign in component done

--- 3-angular/src/Auth/components/sign-in/sign-in.container.ts
+++ 3-angular/src/Auth/components/sign-in/sign-in.container.ts
@@ -1,7 +1,5 @@
 import { AuthSignInActions } from '@auth/store/actions';
-import { AuthSignInSelectors } from '@auth/store/selectors';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 import { AuthExtendedAppState } from '@auth/store/reducers';
 import { Store } from '@ngrx/store';
 import { SignInData } from '@auth/models/sign-in-data.model';
@@ -9,23 +7,9 @@ import { SignInStateService } from './sign-in-state.service';
 
 @Component({
   templateUrl: './sign-in.container.html',
-  styleUrls: ['./sign-in.container.scss'],
   providers: [SignInStateService],
 })
 export class SignInContainerComponent {
-  // username$: Observable<string | undefined> = this.store.select(
-  //   AuthSignInSelectors.getSignInUserName
-  // );
-  // isSignInCommunicating$: Observable<boolean> = this.store.select(
-  //   AuthSignInSelectors.getIsSignInCommunicating
-  // );
-  // isWrongUserNameOrPassword$: Observable<boolean> = this.store.select(
-  //   AuthSignInSelectors.getIsWrongUserNameOrPassword
-  // );
-  // isUserNameJustVerified$: Observable<boolean> = this.store.select(
-  //   AuthSignInSelectors.getIsUserNameJustVerified
-  // );
-
   constructor(
     private store: Store<AuthExtendedAppState>,
     public signInStateService: SignInStateService
